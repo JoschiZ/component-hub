2e1c8830fe40c1d99432366671dca8eafd33d94e
Joschi <joschi.grey@posteo.de>
2023-11-14 17:36:28 +0100

    Added MudCompatibleAbstractValidator

--- ComponentHub/Shared/Auth/RegisterOptions.cs
+++ ComponentHub/Shared/Auth/RegisterOptions.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using ComponentHub.Shared.Helper.Validation;
 using FluentValidation;
 
 namespace ComponentHub.Shared.Auth;
@@ -9,7 +10,7 @@ public sealed class RegisterOptions
 }
 
 
-public sealed class RegisterOptionsValidator : AbstractValidator<RegisterOptions>
+public sealed class RegisterOptionsValidator : MudCompatibleAbstractValidator<RegisterOptions>
 {
     public RegisterOptionsValidator()
     {
@@ -18,4 +19,12 @@ public sealed class RegisterOptionsValidator : AbstractValidator<RegisterOptions
             .MinimumLength(5)
             .MaximumLength(16);
     }
+    
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<RegisterOptions>.CreateWithOptions((RegisterOptions)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
 }
---
+++ ComponentHub/Shared/Helper/Validation/MudCompatibleAbstractValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace ComponentHub.Shared.Helper.Validation;
+
+public abstract class MudCompatibleAbstractValidator<T>: AbstractValidator<T>
+{
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+        return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+    };
+}
